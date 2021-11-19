48fe4eb855b83b717b9ecf962cafd03ee225e619
plimsolls <30596543+liuming666666@users.noreply.github.com>
2021-11-19 11:21:25 +0800

    修改.gitignore文件 添加hello访问方法

--- src/main/java/com/lliuming/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/lliuming/springbootdemo/SpringBootDemoApplication.java
@@ -2,16 +2,24 @@ package com.lliuming.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * springboot入门demo
  */
 @SpringBootApplication
+@RestController
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
-        System.out.println("");
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s", name);
     }
 
 }
