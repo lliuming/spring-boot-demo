d8d180ecb61b23555282e865663d9f07f5dc91ae
plimsolls <30596543+liuming666666@users.noreply.github.com>
2021-11-19 11:10:07 +0800

    Initial commit

---
+++ src/main/java/com/lliuming/springbootdemo/SpringBootDemoApplication.java
@@ -0,0 +1,17 @@
+package com.lliuming.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot入门demo
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+        System.out.println("");
+    }
+
+}
